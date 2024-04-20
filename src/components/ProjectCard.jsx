56e29794996b150509476f32f385127d9909a046
Troy <tydamon@hotmail.com>
2024-04-21 01:42:04 +1000

    added github repo url props for projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,17 @@
 //import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-function ProjectCard({ image, title, description, projectUrl }) {
+
+function ProjectCard({ image, title, description, projectUrl, projectGitUrl }) {
   return (
     <div className="project-card">
 
 <a href={projectUrl} target="_blank" rel="noopener noreferrer">
       <img src={image} alt={title} className="project-image" />
 </a>
+<a href={projectGitUrl} target="_blank" rel="noopener noreferrer">
+         <FontAwesomeIcon icon={faGithub}  size="2x" /> </a>
       <div className="project-info">
         <h3 className="project-title">{title}</h3>
         <p className="project-description">{description}</p>
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -6,93 +6,58 @@ function ProjectList() {
       title: 'PWA Text Editor',
       image: 'path/to/image1.jpg',
       description: 'A Text Editor app built with Javascript and utilizing PWA technologies, including Webpack, a Service Worker, and IndexedDB for use and storage offline, deployed on Render.',
-      projectUrl: 'https://jate-text-editor-10.onrender.com/',   
+      projectUrl: 'https://jate-text-editor-10.onrender.com/', 
+      projectGitUrl: 'https://github.com/sifzerda/JATE-text-editor',  
     },
-    {
-      title: 'MongoDB Social Network API',
-      image: 'path/to/image2.jpg',
-      description: 'Description of Project 2...',
-      projectUrl: 'https://github.com/sifzerda/social-network-api',   
-    },
-    {
-        title: 'Regex Tutorial: Hex Code',
-        image: 'path/to/image2.jpg',
-        description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/regex-tutorial',   
-      },
+
       {
         title: 'MVC Tech Blog',
         image: 'path/to/image2.jpg',
         description: 'Description of Project 2...',
-        projectUrl: 'https://young-brushlands-11834-82a452c6a69a.herokuapp.com/',   
+        projectUrl: 'https://young-brushlands-11834-82a452c6a69a.herokuapp.com/',
+        projectGitUrl: 'https://github.com/sifzerda/Tech-Blog',    
       },
-      {
-        title: 'ORM E-commerce Back End',
-        image: 'path/to/image2.jpg',
-        description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/e-commerce-back-end',   
-      },
-      {
-        title: 'MySQL Employee Tracker',
-        image: 'path/to/image2.jpg',
-        description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/SQL-employee-tracker',   
-      },
+
       {
         title: 'Express Note Taker',
         image: 'path/to/image2.jpg',
         description: 'Description of Project 2...',
-        projectUrl: 'https://mysterious-ravine-39693-dea3bc4026fc.herokuapp.com/',   
+        projectUrl: 'https://mysterious-ravine-39693-dea3bc4026fc.herokuapp.com/', 
+        projectGitUrl: 'https://github.com/sifzerda/note-taker',   
       },
-      {
-        title: 'OOP SVG Logo Maker',
-        image: 'path/to/image2.jpg',
-        description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/svg-logo-maker',   
-      },
-      {
-        title: 'Node Professional README Generator',
-        image: 'path/to/image2.jpg',
-        description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/professional-README-generator',   
-      },
+
       {
         title: 'Server-Side API Weather Dashboard',
         image: 'path/to/image2.jpg',
         description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/weather-dashboard',   
+        projectUrl: 'https://sifzerda.github.io/weather-dashboard/',  
+        projectGitUrl: 'https://github.com/sifzerda/weather-dashboard',  
       },
+
       {
         title: 'Third Party API Work Day Scheduler',
         image: 'path/to/image2.jpg',
         description: 'Description of Project 2...',
-        projectUrl: 'https://sifzerda.github.io/workday-scheduler',   
+        projectUrl: 'https://sifzerda.github.io/workday-scheduler/',  
+        projectGitUrl: 'https://github.com/sifzerda/workday-scheduler',  
       },
+
       {
         title: 'Web API Coding Quiz',
         image: 'path/to/image2.jpg',
         description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/coding-quiz',   
+        projectUrl: 'https://sifzerda.github.io/coding-quiz/',   
+        projectGitUrl: 'https://github.com/sifzerda/coding-quiz', 
       },
-      {
-        title: 'JavaScript Password Generator',
-        image: 'path/to/image2.jpg',
-        description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/js-password-generator',   
-      },
-      {
-        title: 'CSS Professional Portfolio',
-        image: 'path/to/image2.jpg',
-        description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/professional-portfolio',   
-      },
-      {
-        title: 'HTML CSS Git Code Refactor',
-        image: 'path/to/image2.jpg',
-        description: 'Description of Project 2...',
-        projectUrl: 'https://github.com/sifzerda/horiseon-SEO-project',   
-      },
-    // Add more projects as needed
+
+//      {
+//        title: 'JavaScript Password Generator',
+//        image: 'path/to/image2.jpg',
+//        description: 'Description of Project 2...',
+//        projectUrl: 'https://sifzerda.github.io/password-generator/',   
+//        projectGitUrl: 'https://github.com/sifzerda/js-password-generator',
+//      },
+
   ];
 
   return (
@@ -104,6 +69,7 @@ function ProjectList() {
           image={project.image}
           description={project.description}
           projectUrl={project.projectUrl}
+          projectGitUrl={project.projectGitUrl}
         />
       ))}
     </div>
