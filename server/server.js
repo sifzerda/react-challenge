983c5eb45bfdcf8e26524561d08a286d67eb656d
Troy <tydamon@hotmail.com>
2024-06-12 06:24:03 +1000

    changed server serving static files

--- server/server.js
+++ server/server.js
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '../client/dist/index.html'));
 });
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);
